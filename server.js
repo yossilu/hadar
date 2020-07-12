8c713ef70c8dd5bdb2fe8cc40679f513793482c2
Yossi Lufton <yossil153@gmail.com>
2020-07-12 22:45:05 +0300

    heroku changes

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+
+app.use(express.static('./dist/hadar'));
+app.use(cors());
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: './dist/hadar'}
+  );
+});
+
+app.use((req, res, next) => {
+    console.log("ALLOW CORS")
+    // res.header("Access-Control-Allow-Origin", '*');
+    // res.header("Access-Control-Allow-Credentials", true);
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.listen(process.env.PORT || 8080);
+ 
+
+module.exports = app;
